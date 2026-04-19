4597ede0922e894213f65845bc51ce547286e08d
idealana <lanarf97@gmail.com>
2026-04-19 17:23:05 +0700

    feat: update main to add auth routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v3"
+    "go-auth/internal/http/handler"
+    "go-auth/internal/service"
+    "go-auth/internal/repository"
 )
 
 func main() {
+    userRepository := repository.NewUserRepository()
+    
+    authService := service.NewAuthService(userRepository)
+    authHandler := handler.NewAuthHandler(authService)
+
 	app := fiber.New()
 
+    // ROUTES
 	app.Get("/", func(c fiber.Ctx) error {
         return c.JSON(fiber.Map{
         	"message": "Hello World!",
         })
     })
+
+    authHandler.Routes(app)
+    // END ROUTES
 
     app.Listen(":8100")
 }
