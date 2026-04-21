4673a61647167a5caee9022b1c50ce0f2755b8bf
Maulana <57210129+idealana@users.noreply.github.com>
2026-04-21 15:47:36 +0700

    feat: add env module in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"log"
+	"os"
 	"github.com/gofiber/fiber/v3"
     "go-auth/internal/http/handler"
     "go-auth/internal/service"
     "go-auth/internal/repository"
+    "github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	appName := os.Getenv("APP_NAME")
+	
     userRepository := repository.NewUserRepository()
     
     authService := service.NewAuthService(userRepository)
     authHandler := handler.NewAuthHandler(authService)
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		AppName: appName,
+	})
 
     // ROUTES
 	app.Get("/", func(c fiber.Ctx) error {
