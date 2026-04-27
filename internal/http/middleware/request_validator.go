653554e62c6369ee638a35c1d1a7818c80205b7f
Maulana <57210129+idealana@users.noreply.github.com>
2026-04-27 16:16:18 +0700

    fix: update request_validator.go
    fix indentation
    fix method ValidateRequest() and GetRequest()

--- internal/http/middleware/request_validator.go
+++ internal/http/middleware/request_validator.go
@@ -1,51 +1,48 @@
 package middleware
 
 import (
-  "errors"
-  "log"
-  
-  "go-auth/pkg/validator"
-  
-  "github.com/gofiber/fiber/v3"
+	"errors"
+	"log"
+
+	"go-auth/pkg/validator"
+
+	"github.com/gofiber/fiber/v3"
 )
 
-type ctxKey string
-
-const RequestKey ctxKey = "validated_request"
+const RequestKey = "validated_request"
 
 func ValidateRequest[T any]() fiber.Handler {
-  return func(ctx fiber.Ctx) error {
-    var req T
-
-    if err := ctx.Bind().Body(&req); err != nil {
-      var valErr validator.ValidationError
-      if errors.As(err, &valErr) {
-        return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-          "message": "Bad Request",
-          "errors": valErr.Errors,
-        })
-      }
-
-      log.Printf("invalid request: %v", err)
-
-      return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-          "message": "Invalid Request",
-          "errors": map[string]string{},
-        })
-    }
-
-    ctx.Locals(string(RequestKey), &req)
-
-    return ctx.Next()
-  }
+	return func(ctx fiber.Ctx) error {
+		var req T
+		
+		if err := ctx.Bind().Body(&req); err != nil {
+			var valErr validator.ValidationError
+			if errors.As(err, &valErr) {
+				return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+					"message": "Bad Request",
+					"errors": valErr.Errors,
+				})
+			}
+			
+			log.Printf("invalid request: %v", err)
+			
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Invalid Request",
+				"errors": map[string]string{},
+			})
+		}
+		
+		ctx.Locals(RequestKey, &req)
+		return ctx.Next()
+	}
 }
 
 func GetRequest[T any](ctx fiber.Ctx) (*T, bool) {
-	val := ctx.Locals(string(RequestKey)).(*T)
-  if val == nil {
-    return nil, false
-  }
-
-  req, ok := val.(*T)
-  return req, ok
+	val := ctx.Locals(RequestKey)
+	if val == nil {
+		return nil, false
+	}
+	
+	req, ok := val.(*T)
+	return req, ok
 }
