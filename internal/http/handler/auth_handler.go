55fe660eb1b5f92c9cd456a323ae5e959701f154
idealana <lanarf97@gmail.com>
2026-05-03 22:35:12 +0700

    fix: handler auth_handler.go
    add apperror package
    authService type using interface
    validate error type

--- internal/http/handler/auth_handler.go
+++ internal/http/handler/auth_handler.go
@@ -1,51 +1,67 @@
 package handler
 
 import (
-	"github.com/gofiber/fiber/v3"
-	"go-auth/internal/model/web"
-	"go-auth/internal/service"
+    "errors"
+
+    "go-auth/internal/apperror"
+	"go-auth/internal/dto"
+    "go-auth/internal/service"
 	"go-auth/pkg/validator"
+
+    "github.com/gofiber/fiber/v3"
 )
 
-func NewAuthHandler(service *service.AuthService) *AuthHandler {
-	return &AuthHandler{service}
+func NewAuthHandler(authService service.AuthServiceInterface) AuthHandlerInterface {
+	return &AuthHandler{authService}
 }
 
 type AuthHandler struct {
-	service *service.AuthService
+	authService service.AuthServiceInterface
 }
 
 func (handler *AuthHandler) Routes(app *fiber.App) {
 	app.Post("/login", handler.Login)
 }
 
-func (handler *AuthHandler) Login(c fiber.Ctx) error {
-	var req web.LoginRequest
+func (handler *AuthHandler) Login(ctx fiber.Ctx) error {
+	var req dto.LoginRequest
 
-	if err := c.Bind().Body(&req); err != nil {
-		if resultValidate, ok := err.(*validator.ValidationError); ok {
-            return c.Status(400).JSON(fiber.Map{
-                "message": "Bad request.",
-                "errors": resultValidate.Errors,
+	if err := ctx.Bind().Body(&req); err != nil {
+        var valErr *validator.ValidationError
+		if errors.As(err, &valErr) {
+            return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+                "message": "Bad request",
+                "errors": valErr.Errors,
             })
         }
 
-        return c.Status(400).JSON(fiber.Map{
-            "message": "Invalid Request.",
+        return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "message": "Invalid Request",
             "errors": map[string]string{},
         })
     }
 
-    accessToken, refreshToken, err := handler.service.Login(req)
+    result, err := handler.authService.Login(ctx.Context(), req)
 
     if err != nil {
-    	return c.Status(400).JSON(fiber.Map{
-	        "message": err.Error(),
-	    })
+        if errors.Is(err, apperror.ErrInvalidCredentials) {
+            return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+                "message": err.Error(),
+            })
+        }
+    	
+        return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "message": "Internal Service Error",
+        })
     }
 
-    return c.JSON(fiber.Map{
-        "message": "Login Successfully.",
-        "token": accessToken,
+    data := dto.LoginResponse{
+        UserID: result.UserID,
+        AccessToken: result.AccessToken,
+    }
+
+    return ctx.JSON(fiber.Map{
+        "message": "Login Successfully",
+        "data": data,
     })
 }
