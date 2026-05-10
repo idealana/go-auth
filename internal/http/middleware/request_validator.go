008b04e12f6c61a6e8c94c8fa58d28b3bd857a4a
idealana <lanarf97@gmail.com>
2026-05-10 23:10:15 +0700

    fix: request validator middleware
    use logger from package logger
    add RequestValidator struct and constructor
    dynamic requestKey by generic type name
    remove function resolveLogger()
    add function typeNameOf()

--- internal/http/middleware/request_validator.go
+++ internal/http/middleware/request_validator.go
@@ -2,47 +2,44 @@ package middleware
 
 import (
 	"errors"
-	"log/slog"
+	"fmt"
 
 	"go-auth/internal/http/response"
+	"go-auth/internal/logger"
 	"go-auth/pkg/validator"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 type requestKey struct {
-	name string
+	typeName string
 }
 
-var defaultRequestKey = requestKey{"validated_request"}
-
-type Logger interface {
-	Error(message string, args ...any)
+type RequestValidator struct {
+	log logger.Logger
 }
 
-type defaultLogger struct {}
-
-func (logger *defaultLogger) Error(message string, args ...any) {
-	slog.Error(message, args...)
+func NewRequestValidator(log logger.Logger) *RequestValidator {
+	return &RequestValidator{
+		log: log,
+	}
 }
 
-func ValidateRequest[T any](logger ...Logger) fiber.Handler {
-	log := resolveLogger(logger...)
-
+func ValidateRequest[T any](rv *RequestValidator) fiber.Handler {
 	return func(ctx fiber.Ctx) error {
 		var req T
 
 		if err := ctx.Bind().Body(&req); err != nil {
-			return handleBindError(ctx, log, err)
+			return rv.handleBindError(ctx, err)
 		}
 		
-		ctx.Locals(defaultRequestKey, &req)
+		ctx.Locals(requestKey{typeName: typeNameOf[T]()}), &req)
 		return ctx.Next()
 	}
 }
 
 func GetRequest[T any](ctx fiber.Ctx) (*T, bool) {
-	val := ctx.Locals(defaultRequestKey)
+	val := ctx.Locals(requestKey{typeName: typeNameOf[T]()})
 	if val == nil {
 		return nil, false
 	}
@@ -51,19 +48,16 @@ func GetRequest[T any](ctx fiber.Ctx) (*T, bool) {
 	return req, ok
 }
 
-func handleBindError(ctx fiber.Ctx, log Logger, err error) error {
+func (r *RequestValidator) handleBindError(ctx fiber.Ctx, err error) error {
 	var valErr *validator.ValidationError
 	if errors.As(err, &valErr) {
 		return response.BadRequest(ctx, "Bad Request", valErr.Errors)
 	}
 
-	log.Error("failed to bind request body", "error", err)
+	r.log.Error("failed to bind request body", "error", err)
 	return response.BadRequest(ctx, "Invalid Request", map[string]string{})
 }
 
-func resolveLogger(loggers ...Logger) Logger {
-	if len(loggers) > 0 && loggers[0] != nil {
-		return loggers[0]
-	}
-	return &defaultLogger{}
+func typeNameOf[T any]() string {
+	return fmt.Sprintf("%T", *new(T))
 }
