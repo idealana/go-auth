699a5d00ba9e15d28b1c1deaded1984c401dcc55
idealana <lanarf97@gmail.com>
2026-05-17 16:10:37 +0700

    feat: update main.go
    remove package log
    implement logger
    add initialize request validator middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
     "fmt"
-	"log"
     "time"
 
     "go-auth/internal/config"
     "go-auth/internal/http/handler"
+    "go-auth/internal/http/middleware"
+    "go-auth/internal/logger"
     "go-auth/internal/repository"
     "go-auth/internal/security"
     "go-auth/internal/service"
@@ -17,26 +18,34 @@ import (
 )
 
 func main() {
+    log := logger.Resolve()
+
     if err := godotenv.Load(); err != nil {
-        log.Println("no .env file found, using system environment variables")
+        log.Info("no .env file found, using system environment variables")
+    }
+
+    validatorService, err := validator.NewValidator()
+    if err != nil {
+        log.Fatal("failed to initialize validator", "error", err)
     }
 
     appConfig := fiber.Config{
         AppName: config.GetAppName(),
-        StructValidator: validator.NewValidator(),
+        StructValidator: validatorService,
     }
 
     jwtAccessKey, err := config.GetJWTAccessKey()
     if err != nil {
-        log.Fatalf("config error: %v", err)
+        log.Fatal("jwt config error", "error", err)
     }
 
     jwtAccessExpired := time.Minute * time.Duration(config.GetJWTAccessExpired())
     jwtAuth, err := security.NewJWTAuthToken(appConfig.AppName, jwtAccessKey, jwtAccessExpired)
     if err != nil {
-        log.Fatalf("failed to initialize jwt: %v", err)
+        log.Fatal("failed to initialize jwt", "error", err)
     }
 
+    reqValidator := middleware.NewRequestValidator(log)
     bcryptPassword := security.NewBcryptPassword()
 	
     // REPOSITORIES
@@ -46,7 +55,10 @@ func main() {
     authService := service.NewAuthService(userRepository, jwtAuth, bcryptPassword)
 
     // HANDLERS
-    authHandler := handler.NewAuthHandler(authService)
+    authHandler := handler.NewAuthHandler(
+        authService,
+        reqValidator,
+    )
 
     app := fiber.New(appConfig)
 
@@ -63,6 +75,6 @@ func main() {
     port := config.GetAppPort()
 
     if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
-        log.Fatalf("server failed to start: %v", err)
+        log.Fatal("server failed to start", "error", err)
     }
 }
