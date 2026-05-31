0a37bb2ea0155bc68198456542543b5934483ea4
idealana <lanarf97@gmail.com>
2026-05-31 07:48:27 +0700

    refactor: implement gorm and update FindByEmail() to get data from database

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -2,34 +2,39 @@ package repository
 
 import (
 	"context"
+	"errors"
 	
 	"go-auth/internal/apperror"
 	"go-auth/internal/model/domain"
+
+	"gorm.io/gorm"
 )
 
-func NewUserRepository() UserRepositoryInterface {
-	return &UserRepository{}
+func NewUserRepository(db *gorm.DB) UserRepositoryInterface {
+	return &UserRepository{
+		DB: db,
+	}
 }
 
 type UserRepository struct {
+	*gorm.DB
 }
 
 func (repo *UserRepository) FindByEmail(ctx context.Context, email string) (domain.User, error) {
 	var user domain.User
 
-	// dummy data
-	findUser := domain.User{
-		ID: 1,
-		FullName: "Uhuy Hehehe",
-		Email: "uhuy@example.id",
-		Password: "$2a$12$ibAELInQIpUUlM2jlH5KbOCRsTjO61KRPtF./1p0tFGBc/Y3q71Lq",
-	}
+	err := repo.DB.
+		WithContext(ctx).
+		Where(&domain.User{Email: email}).
+		First(&user).
+		Error
 
-	if email != findUser.Email {
-		return user, apperror.ErrNotFound
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return domain.User{}, apperror.ErrNotFound
+		}
+		return domain.User{}, err
 	}
-
-	user = findUser
 
 	return user, nil
 }
